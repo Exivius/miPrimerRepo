2aee5cdd4db2c1d40452bd9d70fe74c5bc2e6a88
felicori <felipco15@hotmail.com>
2026-08-05 22:49:26 -0500

    nuevo gitignore creado

--- proyecto1/probando/obj/Debug/net10.0/probando.AssemblyInfo.cs
+++ proyecto1/probando/obj/Debug/net10.0/probando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794d4264904703e13404a56991d7aec484083b92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b71e14463ca0f3fcdc8f70969b7eebaa415576a")]
 [assembly: System.Reflection.AssemblyProductAttribute("probando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
