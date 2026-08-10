3f01fb84ce501d65f8d0a94443caa01769b50d27
felicori <felipco15@hotmail.com>
2026-08-10 18:10:09 -0500

    Se modifica nuevamente el archivo program.cs y se agrega una nueva linea testingConnection

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Hello, Platzi!");
 
 string stripeApiKey = "sk_test_51N7g0eJ8
+
+string testingConnection="Server=localhost;Database=TestDB;User Id=sa;Password=Your_password123;";
